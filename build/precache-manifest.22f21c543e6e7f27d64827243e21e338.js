c423b1ebf09094839d73192292142042bc89e2d4
ccritch1 <cole.critchfield@students.svu.edu>
2019-04-21 23:01:50 -0400

    first build website

---
+++ build/precache-manifest.22f21c543e6e7f27d64827243e21e338.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b72a94d5187610c7b1cf48071f33caa0",
+    "url": "/static/media/logo_3.b72a94d5.svg"
+  },
+  {
+    "revision": "9b99637b0d9f441a00e4",
+    "url": "/static/css/main.100702d1.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "365cd0e54e2165df1ca3c517bac8b8d2",
+    "url": "/static/media/logo_2.365cd0e5.svg"
+  },
+  {
+    "revision": "84d4c7feb26c8733dd4e",
+    "url": "/static/js/2.609838a9.chunk.js"
+  },
+  {
+    "revision": "9b99637b0d9f441a00e4",
+    "url": "/static/js/main.5934fa44.chunk.js"
+  },
+  {
+    "revision": "c9738eb0d2687814f52601afe97f097b",
+    "url": "/static/media/dribbble.c9738eb0.svg"
+  },
+  {
+    "revision": "1b9323d640fd28219c6cf9deb16e294e",
+    "url": "/static/media/linked.1b9323d6.svg"
+  },
+  {
+    "revision": "5934eebfc972f887baecc6823d3b3639",
+    "url": "/static/media/github.5934eebf.svg"
+  },
+  {
+    "revision": "84d4c7feb26c8733dd4e",
+    "url": "/static/css/2.d91f8fa7.chunk.css"
+  },
+  {
+    "revision": "1ad025889ddf0f47e2f0164534abae46",
+    "url": "/index.html"
+  }
+];
---
+++ src/yes.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+
+class Yes extends Component {
+    render() {
+        retrun(
+            <div className="yessir">
+                <p> hello johnny</p>
+            </div>
+        )
+    }
+}
+
+
+export Yes;
